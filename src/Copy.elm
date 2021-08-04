1583fe04cf866d71ff10e750e02c845844e19f99
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-08-04 13:55:04 -0400

    added small marging change

--- src/Copy.elm
+++ src/Copy.elm
@@ -52,11 +52,11 @@ contributionEmailHref =
 attestation : List (Html msg)
 attestation =
     List.intersperse (br [] []) <|
-        List.map (\n -> div [ Spacing.mt2, Spacing.ml2 ] [ text n ])
-            [ "1.I am at least eighteen years old."
-            , "2.This contribution is made from my own funds, and funds are not being provided to me by another person or entity for the purpose of making this contribution. "
-            , "3.I am a U.S. citizen or lawfully admitted permanent resident (i.e., green card holder)."
-            , "4.I am making this contribution with my own personal credit card and not with a corporate or business credit card or a card issued to another person. "
+        List.map (\n -> div [ Spacing.mt1, Spacing.ml2 ] [ text n ])
+            [ "1. I am at least eighteen years old."
+            , "2. This contribution is made from my own funds, and funds are not being provided to me by another person or entity for the purpose of making this contribution. "
+            , "3. I am a U.S. citizen or lawfully admitted permanent resident (i.e., green card holder)."
+            , "4. I am making this contribution with my own personal credit card and not with a corporate or business credit card or a card issued to another person. "
             ]
 
 
