606887c0cf9fcbb2d0d208ae3473969b1805e6ed
emanegux <dev.evanpiro@gmail.com>
2021-09-21 08:46:49 -0400

    allen changes

--- src/Content/WillSchweitzer.elm
+++ src/Content/WillSchweitzer.elm
@@ -1,5 +1,6 @@
 module Content.WillSchweitzer exposing (arthurEmailAddress, emailBody, emailSubject, firstCopy, firstHeading, promoContent, promoHeading, secondCopy, secondHeading, thirdCopy, thirdHeading)
 
+import Bootstrap.Utilities.Spacing as Spacing
 import Content.Generic exposing (promoCopyFormatter)
 import Html exposing (Html, div, h4, text)
 
@@ -25,7 +26,7 @@ promoHeading =
 
 
 firstHeading =
-    "Let's"
+    "Fight, Wyoming, Fight"
 
 
 firstCopy =
@@ -48,11 +49,32 @@ thirdCopy =
     "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 
+
+--promoContent : Html msg
+--promoContent =
+--    div
+--        []
+--        (promoCopyFormatter firstHeading firstCopy
+--            ++ promoCopyFormatter secondHeading secondCopy
+--            ++ promoCopyFormatter thirdHeading thirdCopy
+--        )
+
+
 promoContent : Html msg
 promoContent =
-    div
-        []
-        (promoCopyFormatter firstHeading firstCopy
-            ++ promoCopyFormatter secondHeading secondCopy
-            ++ promoCopyFormatter thirdHeading thirdCopy
-        )
+    div []
+        [ h4
+            [ Spacing.mt3 ]
+            [ text "Fight, Wyoming, Fight" ]
+        , div
+            []
+            [ div [] [ text "Come on, Cowboys, gold and brown!" ]
+            , div [] [ text "Show them how, boys, hold them down!" ]
+            , div [] [ text "Start right now, boys, don't' delay," ]
+            , div [] [ text "Break away, win today." ]
+            , div [] [ text "Take that ball, and one, two, three!" ]
+            , div [] [ text "Carry on triumphantly -- Come on and fight!" ]
+            , div [] [ text "Fight! Fight, you Cowboys, fight!" ]
+            , div [] [ text "Come on and fight to victory!" ]
+            ]
+        ]
